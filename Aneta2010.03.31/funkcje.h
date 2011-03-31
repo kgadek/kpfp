1ffed4490273f9bc916c1c3a85b886bf8f4abc47
Konrad Gądek <kgadek@gmail.com>
2011-03-31 23:21:09 +0200

    Teraz jest full-poprawnie

--- Aneta2010.03.31/funkcje.h
+++ Aneta2010.03.31/funkcje.h
@@ -1,3 +1,6 @@
+#ifndef FUNKCJE_H_
+#define FUNKCJE_H_
+
 #ifndef NULL
 #define NULL 0
 #endif
@@ -13,4 +16,6 @@ enum CompResult eCompareString(char[], char[]);
 void AppendString(char[], char[]);
 
 void ReplaceCharactersInString(char[], char, char);
+
+#endif
 
