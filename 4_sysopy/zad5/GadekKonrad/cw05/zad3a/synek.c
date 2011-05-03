c26db8f6244c206228326f87e45cc03e9417c678
Konrad Gądek <kgadek@gmail.com>
2011-05-03 22:12:55 +0200

    Zad 5.3a done

--- 4_sysopy/zad5/GadekKonrad/cw05/zad3a/synek.c
+++ 4_sysopy/zad5/GadekKonrad/cw05/zad3a/synek.c
@@ -9,8 +9,36 @@ komunikatu na temat ilości odebranych sygnałów SIGUSR1. */
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
+void handler1(int);
+void handler2(int);
 
-int main(int argc, char **argv) {
+unsigned int cnt;
+
+int main() {
+	struct sigaction aH1;
+	struct sigaction aH2;
+
+	cnt = 0;
+
+	aH1.sa_handler = handler1;
+	sigaction(SIGUSR1, &aH1, NULL);
+	aH2.sa_handler = handler2;
+	sigaction(SIGUSR2, &aH2, NULL);
+
+	while(1)
+		pause();
+
 	return 0;
+}
+
+void handler1(int sigid) {
+	++cnt; /*po cichu udajemy, że to jest atomowe...*/
+}
+
+void handler2(int sigid) {
+	printf("SIGUSR1 cnt=%d\n",cnt);
+	exit(0);
 }
