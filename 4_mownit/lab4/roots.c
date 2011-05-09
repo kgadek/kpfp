409dc3de979ee83d1303c3913410cf173ba5d417
Konrad Gądek <kgadek@gmail.com>
2011-05-09 03:16:28 +0200

    4:mownit:4 niby coś jest ale to coś nie bangla

--- 4_mownit/lab4/roots.c
+++ 4_mownit/lab4/roots.c
@@ -22,6 +22,16 @@ void f_wielomianowa_fdf(double,void*,double*,double*);	/*obie wartości naraz*/
 
 
 int main(/*int argc, char **argv*/) {
+	uint it = 0;							/*kolejna iteracja*/
+	uint maxit = 100;						/*maksymalna ilość iteracji*/
+	const gsl_root_fsolver_type *T;			/*typ solvera (bracketing)*/
+	gsl_root_fsolver *s;					/*solver*/
+	double x_lo = 0.0;						/*bracket-low*/
+	double x_hi = 10.0;						/*bracket-high*/
+	int status;								/*status solvera*/
+	double r;								/*...wg stanu mojej wiedzy pierwiastek jest w R*/
+	double r_exp = 0;						/* (...a pierwiastek jest w R_exp ale ciii...) */
+
 	gsl_function f_kwadratowa_f;
 	gsl_function_fdf f_kwadratowa_fdf;
 	struct f_wielomianowa_params f_kwadratowa_pInst;
@@ -48,6 +58,29 @@ int main(/*int argc, char **argv*/) {
 				dtmp,
 				GSL_FN_EVAL(&f_kwadratowa_f, dtmp),
 				GSL_FN_FDF_EVAL_F(&f_kwadratowa_fdf, dtmp));*/
+
+	T = gsl_root_fsolver_brent;
+	s = gsl_root_fsolver_alloc(T);
+	gsl_root_fsolver_set(s, &f_kwadratowa_f, x_lo, x_hi);
+
+	printf("[bracketing: %s]\n", gsl_root_fsolver_name (s));
+	printf ("%5s [%9s, %9s] %9s %10s %9s\n",
+			"iter", "lower", "upper", "root", "err", "err(est)");
+
+	do {
+		++it;
+		status = gsl_root_fsolver_iterate(s);
+		r = gsl_root_fsolver_root(s);
+		x_lo = gsl_root_fsolver_x_lower(s);
+		x_hi = gsl_root_fsolver_x_upper(s);
+		status = gsl_root_test_interval(x_lo, x_hi, 0, 0.001);
+		if(status == GSL_SUCCESS)
+			printf("ROOTS! BLOODY ROOTS!\n");
+		printf("%5d [%.7f, %.7f] %.7f %+.7f %.7f\n",
+				it, x_lo, x_hi, r, r-r_exp, x_hi-x_lo);
+	} while(status == GSL_CONTINUE && it < maxit);
+
+	gsl_root_fsolver_free(s);
 
 	free(f_kwadratowa_pInst.a);
 	f_kwadratowa_pInst.a = 0;
