58605900bddae38fe1c76e7f53b0f10b21938ce0
Konrad Gądek <kgadek@gmail.com>
2011-06-05 18:52:39 +0200

    Performance test v0++++: even more tweeeeakssss

--- zz_PerfTest/bs.lisp
+++ zz_PerfTest/bs.lisp
@@ -1,5 +1,24 @@
 (declaim (optimize (speed 3) (debug 0) (safety 0) (compilation-speed 0)))
 
+(defun sv-binsearch-inner (K vect a b)
+  (declare (fixnum K a b)
+	   (type (simple-array fixnum (32)) vect)
+	   (optimize (speed 3) (debug 0) (safety 0) (compilation-speed 0)))
+  (if (< b a)
+      -1
+      (let* ((i (floor (+ a b)
+		       2))
+	     (val (aref vect i)))
+	(declare (fixnum i val))
+	(cond ((> K val) (sv-binsearch-inner K vect
+					     (1+ i)
+					     b))
+	      ((< K val) (sv-binsearch-inner K vect
+					     a
+					     (1- i)))
+	      (t i)))))
+	   
+
 (defun sv-binsearch (K vect size)
   (declare (fixnum K)
 	   (type (simple-array fixnum (32)) vect)
@@ -21,6 +40,8 @@
 
 (defun testit()
   (let ((A (make-array 32 :element-type 'fixnum :initial-contents (loop for i below 32 collect i))))
+    (sb-profile:reset)
+    (sb-profile:profile random sv-binsearch-inner sv-binsearch)
     (time (loop
 	     with max of-type fixnum = 10000
 	     with r of-type fixnum = nil
@@ -30,4 +51,8 @@
 	       (loop
 		  for k of-type fixnum below max
 		  do
-		    (setf r (the fixnum (sv-binsearch j A 32))))))))
+		    (setf r (the fixnum
+			      (sv-binsearch-inner j A 0 31)
+			      #+nil(sv-binsearch j A 32)
+				 )))))
+    (sb-profile:report)))
