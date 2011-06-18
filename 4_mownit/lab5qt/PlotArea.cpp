25040c47612a7eff86803c43817eaf6af9d2928b
Konrad Gądek <kgadek@gmail.com>
2011-06-18 21:08:37 +0200

    4 Mownit 5qt : Rysuje wykres

--- 4_mownit/lab5qt/PlotArea.cpp
+++ 4_mownit/lab5qt/PlotArea.cpp
@@ -2,36 +2,87 @@
 #include <QtCore/Qt>
 #include "PlotArea.h"
 
+template<class T> T min(T arg1, T arg2) { return arg1<arg2?arg1:arg2; }
+template<class T> T max(T arg1, T arg2) { return arg1<arg2?arg2:arg1; }
+
+PlotArea::~PlotArea() {
+	if(x) {
+		delete [] x;
+		x = 0;
+	}
+	if(y) {
+		delete [] y;
+		y = 0;
+	}
+}
+
 PlotArea::PlotArea(QWidget *parent) : QWidget(parent) {
-	image = QImage(100,100,QImage::Format_ARGB32);
+	image = QImage(500,300,QImage::Format_ARGB32);
+	d = 0.008;
+	c = 0.07;
+	b = 0.1;
+	a = 0.1;
+	y0 = 1000;
+	x0 = 1;
+	n = 10000;
+	h = 0.1;
+
+	x = new double[n];
+	y = new double[n];
+	xmax = x[0] = x0;
+	y[0] = y0;
+
+	for(int i=0; i+1<n; ++i) {
+		x[i+1] = max(0.0, x[i] + h * ( -a*x[i] + c*d*x[i]*y[i] ));
+		xmax = max(xmax, x[i+1]);
+		y[i+1] = max(0.0, y[i] + h * ( b*y[i] - d*x[i]*y[i] ));
+		xmax = max(xmax, y[i+1]);
+	}
 }
 
 
 void PlotArea::paintEvent(QPaintEvent *) {
-	QPainter painter(this);
-	painter.setRenderHint(QPainter::Antialiasing);
+	QPainter painterX(this);
+	QPainter painterY(this);
+	painterX.setRenderHint(QPainter::Antialiasing);
+	painterY.setRenderHint(QPainter::Antialiasing);
 
-	QLinearGradient grad;
-	grad.setColorAt(0.0, QColor(255,0,0,255));
-	grad.setColorAt(1.0, QColor(255,127,0,255));
-	painter.setBrush(grad);
+	QLinearGradient gradX;
+	QLinearGradient gradY;
 
-//	painter.drawLine(10,10,0,0);
-//	painter.drawLine(10,10,5,5);
+	gradX.setColorAt(0.0, QColor(255,0,0,255));
+	gradX.setColorAt(1.0, QColor(255,127,0,255));
+	painterX.setBrush(gradX);
+	gradY.setColorAt(0.0, QColor(0,255,0,255));
+	gradY.setColorAt(1.0, QColor(0,255,127,255));
+	painterY.setBrush(gradY);
 
-	QPainterPath path;
-	path.lineTo(10,10);
-	path.lineTo(100,0);
+	QPainterPath pathX;
+	QPainterPath pathY;
+	pathX.moveTo(0,image.height());
+	pathY.moveTo(0,image.height());
 
-	path.closeSubpath();
-	painter.drawPath(path);
+	for(int i=0;i<10;++i) {
+		painterX.drawLine((double)i/10.0*image.width(),0,(double)i/10.0*image.width(),image.height());
+		painterX.drawLine(0,(double)i/10.0*image.height(),image.width(),(double)i/10.0*image.height());
+	}
+
+	for(int i=1;i<n;++i) {
+		pathX.lineTo((double)i/n*image.width(), image.height() * (1 - x[i]/xmax));
+		pathY.lineTo((double)i/n*image.width(), image.height() * (1 - y[i]/xmax));
+	}
+	pathX.lineTo(image.width(),image.height());
+	pathY.lineTo(image.width(),image.height());
+
+	pathX.closeSubpath();
+	pathY.closeSubpath();
+
+	painterY.drawPath(pathY);
+	painterX.drawPath(pathX);
+
 }
 
 QSize PlotArea::sizeHint() const {
 	return image.size();
 }
 
-
-void PlotArea::mousePressEvent(QMouseEvent *evt) {
-
-}
--- 4_mownit/lab5qt/PlotArea.h
+++ 4_mownit/lab5qt/PlotArea.h
@@ -3,15 +3,33 @@
 
 #include <QtGui/QtGui>
 
+template<class T> T min(T, T);
+template<class T> T max(T, T);
+
 class PlotArea : public QWidget {
 	Q_OBJECT
 public:
 	PlotArea(QWidget *parent = 0);
+	~PlotArea();
 	QSize sizeHint() const;
 protected:
-	void mousePressEvent(QMouseEvent *);
 	void paintEvent(QPaintEvent *);
 private:
+	double y0; //początkowa ilość ofiar
+	double x0; //początkowa ilość drapieżników
+	int n; //ilość kroków czasowych
+	double h; //krok czasowy
+	double *x; //populacja drapieżników
+			//x_{n+1} = x_n + h (-a x_n + c d x_n y_n)
+	double xmax;
+	double *y; //populacja ofiar
+			//y_{n+1} = y_n + h (b y_n - d x_n y_n)
+	double a; //wsp. śmierci drapieżników z powodu braku ofiar
+	double b; //wsp. narodzin ofiar, gdy nie ma drapieżników
+	double c; //efektywność, z jaką drapieżnik wykorzystuje energię pozyskaną ze zjedzenia ofiar (0, 1]
+	double d; //efektywność uśmiercania ofiar przez drapieżników
+
+
 	QImage image;
 };
 
