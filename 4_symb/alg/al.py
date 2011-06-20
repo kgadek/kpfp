c21a9f8bb06813422b2d0e650c19a256384f6d67
Konrad Gądek <kgadek@gmail.com>
2011-06-20 17:45:44 +0200

    4 Symb: Parsowanie wyrażeń matematycznych v3

--- 4_symb/alg/al.py
+++ 4_symb/alg/al.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
-def oprank(op):
-    if op in ['(', ')']: return 3
-    elif op in ['*',]: return 7
-    elif op in ['+', '-']: return 5
-    
-def pprint(expr,indent=0):
-    if type(expr) == type([]):
-        return '(%s)' % (' '.join(map(lambda x: pprint(x, indent+2), expr)),)
-    return '%s' % (expr,)
-
-operators =['+', '-', '*', '^']
+operators = ['+', '-', '*', '^', '_'] 
 
 def operatorrank(op):
+    """ Zwraca priorytet operatora/funkcji. """
     if op == '+': return 5
     if op == '-': return 5
     if op == '*': return 7
     if op == '^': return 9
+    if op == '_': return 5
 
 def gettok(strin):
+    """ Zwraca pojedynczy token z początku stringu strin. """
     return strin[0]
 
 def arity(operator):
-    return 2
+    """ Zwraca ilość argumentów, jakie przyjmuje funkcja. """
+    return operator == '_' and 1 or 2
 
 def mkexpr(op, args): 
+    """ Tworzy wyrażenie zbudowane z operatora i jego argumentów. """
     return '(%s %s)' % (op, ' '.join(map(lambda x: str(x), args)), )
 
+def preparse(strin):
+    """ Zwraca dobrze określone wyrażenie infiksowe (bez dwuznaczności, np. operator "-"
+    w wyrażeniu (-a) zostaje zamieniony na operator "_" . """
+    return strin
+
 def parse(strin):
-    op = [  ]
+    """ Parsuje dobrze określone wyrażenie infiksowe i zwraca wyrażenie w postaci prefiksowej, uproszczonej. """
+    op = []
     ex = []
     i = 0
     while i < len(strin):
@@ -63,7 +64,7 @@ def parse(strin):
                 ex.append(mkexpr(opt,args))
             op.append(tk)
         i += len(tk)
-    while len(op) > 0:
+    while len(op) > 0: # zredukuj co jeszcze nie zostało zredukowane
         opt = op.pop()
         arty = arity(opt)
         args = ex[-arty:]
@@ -75,11 +76,11 @@ if __name__ == '__main__':
     strs = ['2', '2+2', '(2+2)',
             '(m+n)*(m-n)-(m^2-n^2)',
             '2-2+2', '2+2*2',
-            '2+3*4', '2*3+4', '(2+3)*4', '2+(3*4)', '(2*3)+4', '2*(3+4)'
-            '(a+b+c)*(d-e)*(1-f)*g*6^2*2'
+            '2+3*4', '2*3+4', '(2+3)*4', '2+(3*4)', '(2*3)+4', '2*(3+4)',
+            '(a+b+c)*(d-e)*(1-f)*g*6^2*9*_2'
             ]
     for strin in strs:
         print strin
-        print parse(strin)[0]
+        print parse(preparse(strin))
         print '===================================================================================================='
     
