eacf8027025bcd03174ca00098694817a572cd6a
Konrad Gądek <kgadek@gmail.com>
2011-06-22 03:14:52 +0200

    4 Sysopy 9.3: Kopiec dopracowany (my style) + wstępnie zadanie 9.3ab

--- 4_sysopy/zad9/GadekKonrad/cw09/zad3/heap.c
+++ 4_sysopy/zad9/GadekKonrad/cw09/zad3/heap.c
@@ -1,9 +1,9 @@
 #include "heap.h"
 #include <stdlib.h>
 
-#define LEFT(x) ((x)<<1)
-#define RIGHT(x) (((x)<<1)+1)
-#define PARENT(x) ((x)>>1)
+#define LEFT(x) (((x)<<1)+1)
+#define RIGHT(x) (((x)+1)<<1)
+#define PARENT(x) (((x)-1)>>1)
 
 void kpfp_heap_init(struct kpfp_heap *heap, int maxSize) {
 	heap->size = 0;
@@ -14,23 +14,23 @@ void kpfp_heap_init(struct kpfp_heap *heap, int maxSize) {
 void kpfp_heap_free(struct kpfp_heap *heap) {
 	heap->max = 0;
 	free(heap->tab);
+	head->tab = 0;
 }
 int kpfp_heap_push(struct kpfp_heap *heap, int val) {
-	int i, l;
+	int i, l, p;
 	if(heap->size == heap->max)
 		return val-1;
 	heap->tab[i=(heap->size)++] = val;
-	while(i>0 && heap->tab[PARENT(i)] < heap->tab[i]) { /*swap*/
+	while(i>0 && heap->tab[p=PARENT(i)] < heap->tab[i]) { /*swap*/
 		l = heap->tab[i];
-		heap->tab[i] = heap->tab[PARENT(i)];
-		heap->tab[PARENT(i)] = l;
-		i = PARENT(i);
+		heap->tab[i] = heap->tab[p];
+		heap->tab[i=p] = l;
 	}
 	return val;
 }
 
 int kpfp_heap_popMax(struct kpfp_heap *heap) {
-	int res = 0;
+	int res;
 	if(!heap->size)
 		return 0;
 	res = heap->tab[0];
@@ -51,19 +51,13 @@ void _kpfp_heap_heapify(struct kpfp_heap *heap, int larg) {
 		i = larg;
 		l = LEFT(i);
 		r = RIGHT(i);
-		if(l < heap->size &&
-				heap->tab[l] > heap->tab[i])
+		if(l < heap->size && heap->tab[l] > heap->tab[i])
 			larg = l;
 		else
 			larg = i;
-		if(r < heap->size &&
-				heap->tab[r] > heap->tab[larg]) {
+		if(r < heap->size && heap->tab[r] > heap->tab[larg])
 			larg = r;
-		}
-		if(larg != i) { /*swap*/
-			l = heap->tab[i];
-			heap->tab[i] = heap->tab[larg];
-			heap->tab[larg] = l;
-		}
+		if(larg != i)
+			heap->tab[i] ^= heap->tab[larg] ^= heap->tab[i] ^= heap->tab[larg]; /* można, bo wiemy że mają różne wartości */
 	} while(larg != i);
 }
--- 4_sysopy/zad9/GadekKonrad/cw09/zad3/uno.c
+++ 4_sysopy/zad9/GadekKonrad/cw09/zad3/uno.c
@@ -2,8 +2,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "heap.h"
+#include <pthread.h>
 
+#define KLIENTCZEKANADOSTAWCOW 0
+
+#define GETM(mutex) pthread_mutex_lock(&mutex)
+#define RELM(mutex) pthread_mutex_unlock(&mutex)
+
+void* klient(void*); /* wchodzi i kupuje */
+void* dostawca(void*); /* jest wstydliwy i nie wejdzie do sklepu gdy są w nim klienci; dostarcza sklepowi towarów ale też pobiera należności */
+
+
+int klientCntr; /*ilość klientów w sklepie*/
+pthread_cond_t czysto; /*sygnał, że w sklepie jest czysto*/
+pthread_mutex_t klamka; /*tylko jedna osoba może trzymać klamkę (czyli atomowe wchodzenie/wychodzenie ze sklepu) */
+pthread_mutex_t parking; /*parking dla dostawców*/
 
 int main(int argc, char **argv) {
+	/*init*/
+	pthread_mutex_init(&klamka, NULL);
+	pthread_mutex_init(&parking, NULL);
+	pthread_cond_init(czysto, NULL);
+	klientCntr = 0;
 	return 0;
+}
+
+void* klient(void *arg) {
+#if KLIENTCZEKANADOSTAWCOW
+	GETM(parking);
+	RELM(parking);
+#endif
+	GETM(klamka);
+		++klientCntr;
+	RELM(klamka);
+	// klientowanie
+	GETM(klamka);
+		if(!--klientCntr)
+			pthread_cond_signal(&czysto); /*jeśli wychodząc okaże się, że klient był ostatni to mówi o tym ew. dostawcy (wszyscy w mieście wiedzą, że dostawcy są wstydliwi)*/
+	RELM(klamka);
+}
+
+void* dostawca(void *arg) {
+	GETM(parking); /*blokuj -- dostawcy szukają miejsc parkingowych (jest jedno!), tylko pierwszy zaparkował i może sprawdzać czy jest pusto w sklepie*/
+	GETM(klamka);
+		while(!klientCntr)
+			pthread_cond_wait(&czysto, &klamka);
+		// dostawa
+	RELM(klamka);
+	RELM(parking);
 }
