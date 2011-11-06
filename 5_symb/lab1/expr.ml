e3de3116ce52d81b0ef3144ba1a7e9edd0953d5c
Konrad Gądek <kgadek@gmail.com>
2011-11-06 21:30:07 +0100

    5 OS : zad 1.2 chyba done

--- 5_symb/lab1/expr.ml
+++ 5_symb/lab1/expr.ml
@@ -33,7 +33,6 @@ let rec simplify_expr ex = match ex with
   | Mult(a, b) when a = Int 1 || a = Float 1.0 -> simplify_expr(b)
   | Mult(a, b) when b = Int 1 || b = Float 1.0 -> simplify_expr(a)
   | Add(a,b) -> (let ap = simplify_expr(a) and bp = simplify_expr(b) in
-		 print_string ".";
 		 if ((a <> ap) || (b <> bp)) then
 		   simplify_expr(Add(ap, bp))
 		 else
@@ -43,6 +42,27 @@ let rec simplify_expr ex = match ex with
 		    simplify_expr(Mult(ap, bp))
 		  else
 		    Mult(ap,bp))
+  | Minus(Minus(a)) -> simplify_expr(a)
   | Minus(a) -> Minus(simplify_expr(a))
   | Div(a,b) -> Div(simplify_expr(a), simplify_expr(b))
   | Sub(a,b) -> Sub(simplify_expr(a), simplify_expr(b));;
+
+let print_expr ex =
+  let rec pr_expr ex = match ex with
+    | Int(a) -> string_of_int a
+    | Float(a) -> string_of_float a
+    | Minus(Minus(a)) -> print_expr(a)
+    | Minus(a) -> "-" ^ print_expr(a)
+    | Add(a, Minus(b)) -> print_expr(a) ^ " - " ^ print_expr(b)
+    | Sub(a, Minus(b)) -> print_expr(Add(a,b))
+    | Sub(a, b) -> print_expr(a) ^ " - " ^ print_expr(b)
+    | Mult((Add(a, b) as ab), (Add(c, d) as cd)) -> "( " ^ print_expr(ab) ^ " ) * ( " ^ print_expr(cd) ^ " )"
+    | Mult((Add(b, c) as bc), a)
+    | Mult(a, (Add(b, c) as bc)) -> print_expr(a) ^ " * (" ^ print_expr(bc) ^ ")"
+    | Mult(a, b) -> print_expr(a) ^ " * " ^ print_expr(b)
+    | Div((Add(a, b) as ab), (Add(c, d) as cd)) -> "( " ^ print_expr(ab) ^ " ) / ( " ^ print_expr(cd) ^ " )"
+    | Div((Add(b, c) as bc), a)
+    | Div(a, (Add(b, c) as bc)) -> print_expr(a) ^ " / (" ^ print_expr(bc) ^ ")"
+    | Div(a, b) -> print_expr(a) ^ " / " ^ print_expr(b)
+    | Add(a,b) -> print_expr(a) ^ " + " ^ print_expr(b)
+  in pr_expr(simplify_expr(ex));;
