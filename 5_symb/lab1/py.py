045d928c147d4bc2784b3cb82004886fccd34e2e
Konrad Gądek <kgadek@gmail.com>
2011-11-06 23:04:40 +0100

    5 OS : zad 1.3 done

--- 5_symb/lab1/py.py
+++ 5_symb/lab1/py.py
@@ -2,14 +2,22 @@ def Add(p,q): return ['+', p, q]
 def Sub(p,q): return ['-', p, q]
 def Mult(p,q): return ['*', p, q]
 def Div(p,q): return ['/', p, q]
-def Minus(p): return ['_', p]
+def Minus(p): return ['_', p, 0]
 
 def eval(ex):
     if type(ex) != list: return ex
-    if ex[0] == '_': return -eval(ex[1])
     return {
         '+' : lambda x,y: x+y,
         '-' : lambda x,y: x-y,
         '*' : lambda x,y: x*y,
-        '/' : lambda x,y: x/y
+        '/' : lambda x,y: x/y,
+        '_' : lambda x,y: -x
         }[ex[0]](eval(ex[1]), eval(ex[2]))
+
+def simplify_expr(ex):
+    if type(ex) != list: return ex
+    ex[1] = simplify_expr(ex[1])
+    ex[2] = simplify_expr(ex[2])
+    if ex[0] == '-' and (ex[1] == 0 or ex[2] == 0): return ex[1+int(ex[2]==0)]
+    if ex[0] == '*' and (ex[1] == 1 or ex[2] == 1): return ex[1+int(ex[2]==1)]
+    return ex
