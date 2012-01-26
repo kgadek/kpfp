0c27f770f1c1fafc4c9233ed2c9a1bb22ae19101
Konrad Gądek <kgadek@gmail.com>
2012-01-26 06:58:43 +0100

    5 TW: Lab 10 -- testy TO GO

--- 5_tw/lab10/k.erl
+++ 5_tw/lab10/k.erl
@@ -1,6 +1,6 @@
 -module(k).
 -author("Konrad Gadek").
--export([testm/2, testpm/2]).
+-compile(export_all).
 
 fib(0) -> 0;
 fib(1) -> 1;
@@ -18,4 +18,18 @@ testpm(N,M) ->
       fun(X) -> fib(X) end,
       lists:map(fun(X) -> X rem M end,
 		lists:seq(0,N))).
-			   
+
+fA([],_, _) ->
+    ok;
+fA([A1|A2],B, Func) ->
+    fB(A1, B, Func),
+    fA(A2, B, Func).
+
+fB(_, [], _) ->
+    ok;
+fB(A, [B1|B2], Func) ->
+    {T1, _} = timer:tc(Func, [A, B1]),
+    {T2, _} = timer:tc(Func, [A, B1]),
+    {T3, _} = timer:tc(Func, [A, B1]),
+    io:format("(~p,~p,~p) = ~.3f ~n", [A, B1, Func, (T1 + T2 + T3)/3]),
+    fB(A,B2, Func).
