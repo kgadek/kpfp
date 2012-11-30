ef010a373d9e48c482dcc3e3d23be0275a5488dd
Konrad Gądek <kgadek@gmail.com>
2012-11-30 19:50:18 +0100

    Chyba bangla; bez GC

--- 7_amwj/interpreter.py
+++ 7_amwj/interpreter.py
@@ -15,6 +15,13 @@ t_HEAPANALYSE = r'HeapAnalyze' # GB > USA
 t_COLLECT     = r'Collect'
 t_PRINT       = r'Print'
 
+class Type(object):
+	NULL=0
+	VART=1
+	VARS=2
+	INT=3
+	STR=4
+	REF=5
 
 ########################################################################################################################
 ########################################################################################################################
@@ -40,7 +47,7 @@ def t_INTEGERCONST(t):
 
 def t_NULL(t):
 	r'NULL'
-	t.value = 0
+	t.value = (Type.NULL, 0)
 	return t
 
 
@@ -49,65 +56,134 @@ def t_NULL(t):
 ########################################################################################################################
 
 
+heaphead  = 1
+heapsize  = 81
+heap      = [0 for i in range(heapsize)]
+heaphalf  = heapsize / 2
+heapend   = heaphalf + 1
+
 variables = {}
 
-class SDeref(object):
-	def __init__(self, p, q):
-		self.p = p
-		self.q = q
-	def __str__(self): return "(Deref %s.%s)" % (self.p,self.q)
-	def run(self):
-		print "%s -> %s" % (p, q)	
+class NotEnoughMemoryException(Exception): pass
+class NullPointerException(Exception): pass
+
+def allocate(asize):
+	global heaphead
+	if heaphead + asize > heapend:
+		gc()
+		if heaphead + asize > heapend:
+			raise NotEnoughMemoryException()
+	ret = heaphead
+	heaphead = heaphead + asize
+	return ret
+
+def gc():
+	pass
 
 class SPrintLiteral(object):
+	def __str__(self): return "(Print '%s')" % (self.literal,)
 	def __init__(self, literal):
 		self.literal = literal
-	def __str__(self): return "(Print '%s')" % (self.literal,)
-	def run(self):
-		print self.literal
+	def run(self):     print self.literal
 
 class SPrintVar(object):
+	def __str__(self): return "(Print %s)" % (self.varname,)
 	def __init__(self, varname):
 		self.varname = varname
-	def __str__(self): return "(Print %s)" % (self.varname,)
 	def run(self):
-		print variables[self.varname]
+		ptr = variables[self.varname]
+		if ptr == 0: print "NULL"
+		elif heap[ptr] == Type.VART: print "T{f1={0}, f2={1}, data={2}}".format(heap[ptr+1], heap[ptr+2], heap[ptr+3])
+		elif heap[ptr] == Type.VARS: print "S:'{0}'".format(''.join(heap[ptr+2:ptr+2+heap[ptr+1]]))
 
 class SVarTDecl(object):
+	def __str__(self): return "(VarDeclT %s)" % (self.varname,)
 	def __init__(self, varname):
 		self.varname = varname
-	def __str__(self): return "(VarDeclT %s)" % (self.varname,)
 	def run(self):
-		variables[self.varname] = self
+		ptr = allocate(4)
+		heap[ptr:ptr+4] = [Type.VART,0,0,0]
+		variables[self.varname] = ptr
 
 class SVarSDecl(object):
+	def __str__(self): return "(VarDeclS %s = %s)" % (self.varname,self.value)
 	def __init__(self, varname, value):
 		self.varname = varname
 		self.value = value
-	def __str__(self): return "(VarDeclS %s = %s)" % (self.varname,self.value)
 	def run(self):
-		variables[self.varname] = self
+		if self.value == 0:
+			variables[self.varname] = 0
+		else:
+			ptr = allocate(2+len(self.value))
+			heap[ptr] = Type.VARS
+			heap[ptr+1] = len(self.value)
+			heap[ptr+2:ptr+2+len(self.value)] = self.value
+			variables[self.varname] = ptr
+
+def getRef(ref):
+	if ref[0] == Type.INT:
+		return ref[1]
+	elif ref[0] == Type.STR:
+		return ref[0]
+	else:
+		ptr = variables[ref[1]]
+		ptrv = ptr
+		for x in ref[2]:
+			assert ptr != 0;
+			assert heap[ptrv] == Type.VART
+			if   x == 'f1':
+				ptr = ptrv+1
+				ptrv = heap[ptr]
+			elif x == 'f2':
+				ptr = ptrv+2
+				ptrv = heap[ptr]
+			elif x == 'data':
+				ptr = ptrv+3
+				if ptr < len(heap): ptrv = heap[ptr]
+				else:               ptrv = 0
+		print "GetRef {0} --> ptr={1}".format(ref,ptr)
+		return ptr
 
 class SAssignment(object):
+	def __str__(self): return "(Assignment %s = %s)" % (self.varname,self.value)
 	def __init__(self, varname, value):
 		self.varname = varname
 		self.value = value
-	def __str__(self): return "(Assignment %s = %s)" % (self.varname,self.value)
 	def run(self):
-		variables[self.varname] = self.value
+		print "Assignment {0}::{1}   =   {2}::{3}".format(self.varname, type(self.varname), self.value, type(self.value))
+		lval = getRef(self.varname)
+		rval = getRef(self.value)
+		if type(rval) == str:
+			nptr = allocate(2+len(rval))
+			heap[nptr] = Type.VARS
+			heap[nptr+1] = len(rval)
+			heap[nptr+2:nptr+2+len(rval)] = rval
+			variables[lval] = nptr
+		else:
+			assert lval != 0
+			if type(rval) == int:
+				heap[lval] = rval
+			else:
+				heap[lval] = heap[rval]
 
 class SHeapAnalyse(object):
 	def __str__(self): return "(HeapAnalyse)"
-	def run(self): pass
+	def run(self):
+		print "Heap={0} # {1}\nHeapHead={2}\nVariables={3}".format(
+			zip(range(heaphead),heap[:heaphead]),
+			zip(range(heaphead,100000), heap[heaphead:]),
+			heaphead,
+			variables)
 
 class SCollect(object):
 	def __str__(self): return "(Collect)"
-	def run(self): pass
+	def run(self):
+		print "Here run GC"
+
 
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
-
 
 def p_program(p):
   """program : statements"""
@@ -158,14 +234,21 @@ def p_rvalue(p):
 						| NULL
 						| '"' STRINGCONST '"'
 						| INTEGERCONST"""
-	if   len(p) == 4: p[0] = p[2]
-	elif len(p) == 2: p[0] = p[1]
+	if len(p) == 4:
+		p[0] = (Type.STR, p[2])
+	elif len(p) == 2:
+		if type(p[1]) == tuple:
+			p[0] = p[1]
+		elif type(p[1]) == int:
+			p[0] = (Type.INT, p[1])
 
 def p_deref(p):
 	"""deref : STRINGCONST
 					 | deref '.' STRINGCONST"""
-	if   len(p) == 2: p[0] = p[1]
-	elif len(p) == 4: p[0] = SDeref(p[1], p[3])
+	if   len(p) == 2:
+		p[0] = (Type.REF, p[1], [])
+	elif len(p) == 4:
+		p[0] = (Type.REF, p[1][1], p[1][2]+[p[3]])
 
 def p_error(p):
 	print "Syntax error in line %d" %  p.lineno
@@ -181,7 +264,6 @@ if __name__ == '__main__':
   parser = yacc.yacc()
   with open(sys.argv[1], "r") as fh:
   	result = parser.parse(fh.read(), lexer=lexer)
-  	print result
   	for i in result:
-  		print i
-  		i.run() 
+  		print "\n\n>>> {0}".format(i)
+  		print "<<< {0}".format(i.run())
